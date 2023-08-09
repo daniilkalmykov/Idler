cfc76d703a0b3ce4c5046d394df5bf898a079f20
daniilkalmykov <daniilk24960@gmail.com>
2023-08-09 21:52:38 +0600

    add distance transition

--- Assets/Source/StateMachineSystem/EnemyStateMachine/States/MoveState.cs
+++ Assets/Source/StateMachineSystem/EnemyStateMachine/States/MoveState.cs
@@ -3,7 +3,7 @@ using Source.EnemyView;
 using Source.MovementSystem;
 using UnityEngine;
 
-namespace Source.StateMachineSystem.EnemyStateMachine
+namespace Source.StateMachineSystem.EnemyStateMachine.States
 {
     internal sealed class MoveState : IState
     {
---
+++ Assets/Source/StateMachineSystem/EnemyStateMachine/Transitions/DistanceTransition.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+namespace Source.StateMachineSystem.EnemyStateMachine.Transitions
+{
+    internal sealed class DistanceTransition : ITransition
+    {
+        private readonly Transform _enemy;
+        private readonly Vector3 _target;
+        private readonly float _distanceToTransit;
+        
+        public DistanceTransition(IState nextState, Transform enemy, Vector3 target, float distanceToTransit)
+        {
+            NextState = nextState ?? throw new ArgumentNullException();
+            _enemy = enemy;
+            _target = target;
+
+            if (_distanceToTransit < 0)
+                throw new ArgumentException();
+            
+            _distanceToTransit = distanceToTransit;
+        }
+
+        public IState NextState { get; }
+        
+        public bool CanTransit()
+        {
+            return Vector3.Distance(_enemy.position, _target) <= _distanceToTransit;
+        }
+    }
+}
