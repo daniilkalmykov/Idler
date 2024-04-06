3bb14f76724034a2de782c71e17e8cdb65af5161
depressedbiba <daniil.kalmykov@gtapstudio.com>
2024-04-06 22:40:31 +0500

    refactor: TransformDirectionMovement.cs

--- Assets/Game/Scripts/Runtime/Models/Movement/TransformDirectionMovement.cs
+++ Assets/Game/Scripts/Runtime/Models/Movement/TransformDirectionMovement.cs
@@ -5,7 +5,7 @@ namespace Game.Scripts.Runtime.Models.Movement
 {
     internal sealed class TransformDirectionMovement : IMovable
     {
-        private float _speed;
+        private readonly float _speed;
         private readonly Transform _transform;
 
         public TransformDirectionMovement(float speed, Transform transform)
